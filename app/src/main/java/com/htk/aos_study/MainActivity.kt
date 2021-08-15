df90372e6655ec9b1dfa321aa0140b6a8e2dc1c3
hotaek <kht072623@naver.com>
2021-08-15 16:21:19 +0900

    #2 [CHORE] baseActivity 및 Fragment 및 viewBInding 세팅

--- app/src/main/java/com/htk/aos_study/MainActivity.kt
+++ app/src/main/java/com/htk/aos_study/MainActivity.kt
@@ -2,10 +2,16 @@ package com.htk.aos_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.htk.aos_study.databinding.ActivityMainBinding
+import com.htk.aos_study.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>({ActivityMainBinding.inflate(it)}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
